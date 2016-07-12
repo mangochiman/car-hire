8cbc087cf533125b1291b12acf2579b2469095d7
mangochiman <mangochiman@gmail.com>
2016-07-12 20:24:20 +0200

    Working on admin pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,15 +4,15 @@ class PagesController < ApplicationController
   end
 
   def our_services
-
+    @our_services_page = Page.find_by_page_type('our_services')
   end
 
   def about_us
-
+    @about_us_page = Page.find_by_page_type('about_us')
   end
 
   def our_mission
-
+    @our_mission_page = Page.find_by_page_type('our_mission')
   end
 
   def gallery
@@ -20,7 +20,7 @@ class PagesController < ApplicationController
   end
 
   def contact_us
-
+    @contact_us_page = Page.find_by_page_type('contact_us')
   end
   
 end
