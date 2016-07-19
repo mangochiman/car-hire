f4eaf5cdb84cea26f09d95446053342e81cf82d3
mangochiman <mangochiman@gmail.com>
2016-07-19 20:17:22 +0200

    added layout for the pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -7,26 +7,66 @@ class PagesController < ApplicationController
     @fax = Setting.find_by_key('fax').value rescue ''
     @company_description = Page.find_by_page_type('company_description').content rescue ''
     @media = Car.all
+    render :layout => "main"
   end
 
   def our_services
     @our_services_page = Page.find_by_page_type('our_services')
+    @email = Setting.find_by_key('email').value rescue ''
+    @office_phone = Setting.find_by_key('office_phone').value rescue ''
+    @postal_address = Setting.find_by_key('postal_address').value rescue ''
+    @strengths = Setting.find_by_key('strengths').value rescue ''
+    @fax = Setting.find_by_key('fax').value rescue ''
+    @company_description = Page.find_by_page_type('company_description').content rescue ''
+    @media = Car.all
+    render :layout => "main"
   end
 
   def about_us
     @about_us_page = Page.find_by_page_type('about_us')
+    @email = Setting.find_by_key('email').value rescue ''
+    @office_phone = Setting.find_by_key('office_phone').value rescue ''
+    @postal_address = Setting.find_by_key('postal_address').value rescue ''
+    @strengths = Setting.find_by_key('strengths').value rescue ''
+    @fax = Setting.find_by_key('fax').value rescue ''
+    @company_description = Page.find_by_page_type('company_description').content rescue ''
+    @media = Car.all
+    render :layout => "main"
   end
 
   def our_mission
     @our_mission_page = Page.find_by_page_type('our_mission')
+    @email = Setting.find_by_key('email').value rescue ''
+    @office_phone = Setting.find_by_key('office_phone').value rescue ''
+    @postal_address = Setting.find_by_key('postal_address').value rescue ''
+    @strengths = Setting.find_by_key('strengths').value rescue ''
+    @fax = Setting.find_by_key('fax').value rescue ''
+    @company_description = Page.find_by_page_type('company_description').content rescue ''
+    @media = Car.all
+    render :layout => "main"
   end
 
   def gallery
-
+    @email = Setting.find_by_key('email').value rescue ''
+    @office_phone = Setting.find_by_key('office_phone').value rescue ''
+    @postal_address = Setting.find_by_key('postal_address').value rescue ''
+    @strengths = Setting.find_by_key('strengths').value rescue ''
+    @fax = Setting.find_by_key('fax').value rescue ''
+    @company_description = Page.find_by_page_type('company_description').content rescue ''
+    @media = Car.all
+    render :layout => "main"
   end
 
   def contact_us
     @contact_us_page = Page.find_by_page_type('contact_us')
+    @email = Setting.find_by_key('email').value rescue ''
+    @office_phone = Setting.find_by_key('office_phone').value rescue ''
+    @postal_address = Setting.find_by_key('postal_address').value rescue ''
+    @strengths = Setting.find_by_key('strengths').value rescue ''
+    @fax = Setting.find_by_key('fax').value rescue ''
+    @company_description = Page.find_by_page_type('company_description').content rescue ''
+    @media = Car.all
+    render :layout => "main"
   end
   
 end
